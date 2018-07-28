e71db82304393dc2fc87a414cacd14cae0c0cb62
andresr <aferca@hotmail.es>
2018-07-28 15:55:18 -0500

    commit inicial

---
+++ src/pages/home/home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+
+   usuario:String = ""
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+
+
+}
